82f2aa607000dd83b350bbb365b088e2345fc14a
Afonso Oliveira <I230065@inete.net>
2025-10-27 16:57:28 +0000

    ListView e TreeView com Design melhor

--- File-Explorer/File-Explorer.Designer.cs
+++ File-Explorer/File-Explorer.Designer.cs
@@ -29,73 +29,124 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.lblPath = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.tvMain = new System.Windows.Forms.TreeView();
+            this.txtPath = new System.Windows.Forms.TextBox();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.tcMain = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
             this.lvMain = new System.Windows.Forms.ListView();
             this.Nome = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Data = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Tipo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Tamanho = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tvMain = new System.Windows.Forms.TreeView();
+            this.btnAnterior = new System.Windows.Forms.Button();
+            this.btnDepois = new System.Windows.Forms.Button();
+            this.pbRename = new System.Windows.Forms.PictureBox();
+            this.pbRemove = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.tcMain.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.panel2.SuspendLayout();
-            this.panel3.SuspendLayout();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRename)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRemove)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.lblPath);
-            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.pbRename);
+            this.panel1.Controls.Add(this.pbRemove);
+            this.panel1.Controls.Add(this.btnDepois);
+            this.panel1.Controls.Add(this.btnAnterior);
+            this.panel1.Controls.Add(this.txtPath);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(764, 81);
+            this.panel1.Size = new System.Drawing.Size(876, 49);
             this.panel1.TabIndex = 0;
             // 
-            // lblPath
+            // txtPath
             // 
-            this.lblPath.AutoSize = true;
-            this.lblPath.Location = new System.Drawing.Point(20, 15);
-            this.lblPath.Name = "lblPath";
-            this.lblPath.Size = new System.Drawing.Size(34, 16);
-            this.lblPath.TabIndex = 4;
-            this.lblPath.Text = "Path";
+            this.txtPath.Location = new System.Drawing.Point(374, 16);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(491, 22);
+            this.txtPath.TabIndex = 3;
             // 
-            // textBox1
+            // panel3
             // 
-            this.textBox1.Location = new System.Drawing.Point(70, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(491, 22);
-            this.textBox1.TabIndex = 3;
-            this.textBox1.Text = "C:\\";
+            this.panel3.Location = new System.Drawing.Point(410, 375);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(373, 273);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.tcMain);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(890, 529);
+            this.panel4.TabIndex = 3;
+            // 
+            // tcMain
+            // 
+            this.tcMain.Controls.Add(this.tabPage1);
+            this.tcMain.Controls.Add(this.tabPage2);
+            this.tcMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tcMain.Location = new System.Drawing.Point(0, 0);
+            this.tcMain.Name = "tcMain";
+            this.tcMain.Padding = new System.Drawing.Point(20, 4);
+            this.tcMain.SelectedIndex = 0;
+            this.tcMain.Size = new System.Drawing.Size(890, 529);
+            this.tcMain.TabIndex = 1;
+            this.tcMain.SelectedIndexChanged += new System.EventHandler(this.tcMain_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.panel5);
+            this.tabPage1.Controls.Add(this.panel2);
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 27);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(882, 498);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 27);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(65, 3, 3, 3);
+            this.tabPage2.Size = new System.Drawing.Size(882, 498);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "+";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // panel2
             // 
             this.panel2.Controls.Add(this.tvMain);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel2.Location = new System.Drawing.Point(0, 81);
+            this.panel2.Location = new System.Drawing.Point(3, 52);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(250, 398);
+            this.panel2.Size = new System.Drawing.Size(230, 443);
             this.panel2.TabIndex = 1;
             // 
-            // tvMain
+            // panel5
             // 
-            this.tvMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tvMain.Location = new System.Drawing.Point(0, 0);
-            this.tvMain.Name = "tvMain";
-            this.tvMain.Size = new System.Drawing.Size(250, 398);
-            this.tvMain.TabIndex = 0;
-            // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.lvMain);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel3.Location = new System.Drawing.Point(250, 81);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(514, 398);
-            this.panel3.TabIndex = 2;
+            this.panel5.Controls.Add(this.lvMain);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(233, 52);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(646, 443);
+            this.panel5.TabIndex = 2;
             // 
             // lvMain
             // 
@@ -108,29 +159,113 @@
             this.lvMain.HideSelection = false;
             this.lvMain.Location = new System.Drawing.Point(0, 0);
             this.lvMain.Name = "lvMain";
-            this.lvMain.Size = new System.Drawing.Size(514, 398);
+            this.lvMain.Size = new System.Drawing.Size(646, 443);
             this.lvMain.TabIndex = 0;
             this.lvMain.UseCompatibleStateImageBehavior = false;
+            this.lvMain.View = System.Windows.Forms.View.Details;
+            this.lvMain.SelectedIndexChanged += new System.EventHandler(this.lvMain_SelectedIndexChanged);
+            this.lvMain.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lvMain_MouseDoubleClick);
             // 
             // Nome
             // 
-            this.Nome.Width = 100;
+            this.Nome.Text = "Nome";
+            this.Nome.Width = 200;
+            // 
+            // Data
+            // 
+            this.Data.Text = "Data de Modificação";
+            this.Data.Width = 150;
+            // 
+            // Tipo
+            // 
+            this.Tipo.Text = "Tipo";
+            this.Tipo.Width = 110;
+            // 
+            // Tamanho
+            // 
+            this.Tamanho.Text = "Tamanho";
+            this.Tamanho.Width = 70;
+            // 
+            // tvMain
+            // 
+            this.tvMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvMain.Location = new System.Drawing.Point(0, 0);
+            this.tvMain.Name = "tvMain";
+            this.tvMain.Size = new System.Drawing.Size(230, 443);
+            this.tvMain.TabIndex = 0;
+            this.tvMain.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.tvMain_BeforeExpand);
+            this.tvMain.AfterExpand += new System.Windows.Forms.TreeViewEventHandler(this.tvMain_AfterSelect);
+            // 
+            // btnAnterior
+            // 
+            this.btnAnterior.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnAnterior.Enabled = false;
+            this.btnAnterior.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btnAnterior.Location = new System.Drawing.Point(0, 0);
+            this.btnAnterior.Name = "btnAnterior";
+            this.btnAnterior.Size = new System.Drawing.Size(116, 49);
+            this.btnAnterior.TabIndex = 5;
+            this.btnAnterior.Text = "<";
+            this.btnAnterior.UseVisualStyleBackColor = true;
+            // 
+            // btnDepois
+            // 
+            this.btnDepois.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnDepois.Enabled = false;
+            this.btnDepois.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btnDepois.Location = new System.Drawing.Point(116, 0);
+            this.btnDepois.Name = "btnDepois";
+            this.btnDepois.Size = new System.Drawing.Size(114, 49);
+            this.btnDepois.TabIndex = 6;
+            this.btnDepois.Text = ">";
+            this.btnDepois.UseVisualStyleBackColor = true;
+            // 
+            // pbRename
+            // 
+            this.pbRename.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pbRename.Enabled = false;
+            this.pbRename.Image = global::File_Explorer.Properties.Resources._5136886;
+            this.pbRename.Location = new System.Drawing.Point(298, 0);
+            this.pbRename.Name = "pbRename";
+            this.pbRename.Size = new System.Drawing.Size(68, 49);
+            this.pbRename.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbRename.TabIndex = 8;
+            this.pbRename.TabStop = false;
+            this.pbRename.Click += new System.EventHandler(this.pbRename_Click);
+            // 
+            // pbRemove
+            // 
+            this.pbRemove.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pbRemove.Enabled = false;
+            this.pbRemove.Image = global::File_Explorer.Properties.Resources._860829;
+            this.pbRemove.Location = new System.Drawing.Point(230, 0);
+            this.pbRemove.Name = "pbRemove";
+            this.pbRemove.Size = new System.Drawing.Size(68, 49);
+            this.pbRemove.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbRemove.TabIndex = 7;
+            this.pbRemove.TabStop = false;
+            this.pbRemove.Click += new System.EventHandler(this.pbRemove_Click);
             // 
             // FileExplorer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(764, 479);
+            this.ClientSize = new System.Drawing.Size(890, 529);
+            this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
             this.Name = "FileExplorer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "File Explorer";
             this.Load += new System.EventHandler(this.FileExplorer_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.tcMain.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbRename)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRemove)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -138,16 +273,24 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label lblPath;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.TreeView tvMain;
+        private System.Windows.Forms.TextBox txtPath;
         private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TabControl tcMain;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.ListView lvMain;
         private System.Windows.Forms.ColumnHeader Nome;
         private System.Windows.Forms.ColumnHeader Data;
         private System.Windows.Forms.ColumnHeader Tipo;
         private System.Windows.Forms.ColumnHeader Tamanho;
+        private System.Windows.Forms.TreeView tvMain;
+        private System.Windows.Forms.Button btnDepois;
+        private System.Windows.Forms.Button btnAnterior;
+        private System.Windows.Forms.PictureBox pbRename;
+        private System.Windows.Forms.PictureBox pbRemove;
     }
 }
 
--- File-Explorer/File-Explorer.cs
+++ File-Explorer/File-Explorer.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace File_Explorer
 {
@@ -16,15 +18,199 @@ namespace File_Explorer
         {
             InitializeComponent();
         }
+        public void CriarTab(TabPage aba)
+        {
+            TabPage novaAba = new TabPage();
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
+            Panel panelCima = new Panel();
+            panelCima.Dock = DockStyle.Top;
+            novaAba.Controls.Add(panelCima);
+    
+            //tree
+            Panel panelTree = new Panel();
+            panelTree.Dock = DockStyle.Left;
+            TreeView tree = new TreeView();
+            tree.Dock = DockStyle.Fill;
+            panelTree.Controls.Add(tree);
+            novaAba.Controls.Add(panelTree);
+
+            //list
+            Panel panelList = new Panel();
+            panelList.Dock = DockStyle.Fill;
+            ListView list = new ListView();
+            list.Dock = DockStyle.Fill;
+            panelList.Controls.Add(list);
+            novaAba.Controls.Add(panelList);
+
+            tcMain.TabPages.Insert(tcMain.TabPages.Count - 1,novaAba);
+            tcMain.SelectedTab = novaAba;
+        }
+        public void PopularTreeView()
+        {
+            tvMain.Nodes.Clear();
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                TreeNode node = new TreeNode(drive);
+                node.Tag = drive;
+                node.Nodes.Add("Loading...");
+                tvMain.Nodes.Add(node);
+            }
+        }
+        public string Tamanhos(long tamanho)
+        {
+            if(tamanho > 1000)
+            {
+                long kb = tamanho / 1000;
+                if(kb > 1000)
+                {
+                    long mb = kb / 1000;
+                    if(mb > 1000)
+                    {
+                        long gb = mb / 1000;
+                        return gb + " GB";
+                    }
+                    return mb + " MB";
+                }
+                return kb + " KB";
+            }
+            return tamanho + " bytes";
+        }
+        private void FileExplorer_Load(object sender, EventArgs e)
+        {
+            PopularTreeView();
+        }
+
+        private void tvMain_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            TreeNode node = e.Node; //pega o node selecionado
+            node.Nodes.Clear(); //tira o loading
+            try
+            {
+                string[] directories = Directory.GetDirectories(node.Tag.ToString()); //array de todos os subdiretórios
+                foreach (string directory in directories)
+                {
+                    TreeNode subNode = new TreeNode(Path.GetFileName(directory));
+                    subNode.Tag = directory;
+                    subNode.Nodes.Add("Loading...");
+                    node.Nodes.Add(subNode);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Um Erro ocorreu com " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void tvMain_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string path = e.Node.Tag.ToString();
+            txtPath.Text = path;
+            lvMain.Items.Clear();
+            try
+            {
+                string[] files = Directory.GetFiles(path);
+                foreach (string file in files)
+                {
+                    ListViewItem item = new ListViewItem(Path.GetFileName(file));
+                    item.Tag = file;
+                    item.SubItems.Add(new FileInfo(file).LastWriteTime.ToString());
+                    item.SubItems.Add(new FileInfo(file).Extension.ToString());
+                    string size = Tamanhos(new FileInfo(file).Length).ToString();
+                    item.SubItems.Add(size);
+                    lvMain.Items.Add(item);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Um Erro ocorreu com " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            try
+            {
+                string[] directories = Directory.GetDirectories(path);
+                foreach (string directorie in directories)
+                {
+                    DirectoryInfo dir = new DirectoryInfo(directorie);
+                    ListViewItem item = new ListViewItem(dir.Name);
+                    item.Tag = directorie;
+                    item.SubItems.Add(new DirectoryInfo(directorie).LastWriteTime.ToString());
+                    item.SubItems.Add(new DirectoryInfo(directorie).Extension.ToString());
+                    item.SubItems.Add("");
+                    lvMain.Items.Add(item);
+                }
+            }
+            catch( Exception ex)
+            {
+                MessageBox.Show("Um Erro ocorreu com " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void lvMain_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if(lvMain.SelectedItems.Count > 0)
+            {
+                string filepath = lvMain.SelectedItems[0].Tag.ToString();
+                try
+                {
+                    System.Diagnostics.Process.Start(filepath);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Um Erro ocorreu com " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void tcMain_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(tcMain.TabPages.Count == 1)
+            {
+                this.Close();
+            }
+
+            if(tcMain.SelectedTab.Text == "+")
+            {
+                if (tcMain.TabPages.Count > 1)
+                {
+                    TabPage abaAnterior = tcMain.TabPages[tcMain.TabPages.Count - 1];
+                    CriarTab(abaAnterior);
+                }
+                else
+                {
+                    
+                }
+            }
+        }
+
+        private void pbRemove_Click(object sender, EventArgs e)
+        {
+            foreach(ListViewItem item in lvMain.SelectedItems)
+            {
+                string path = item.Tag.ToString();
+                if(File.Exists(path))
+                    File.Delete(path);
+                if(Directory.Exists(path))
+                    Directory.Delete(path, true);
+            }
+        }
+
+        private void pbRename_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void FileExplorer_Load(object sender, EventArgs e)
+        private void lvMain_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            if(lvMain.SelectedItems.Count == 1)
+            {
+                pbRemove.Enabled = true;
+                pbRename.Enabled = true;
+            }
+            else
+            {
+                pbRename.Enabled = false;
+                pbRemove.Enabled = false;
+            }
         }
     }
 }
